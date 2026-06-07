b3962e7bf70ea8eec9eea591de57656b1a21df38
Dimterion <dimterion59@gmail.com>
2026-06-07 09:59:21 +0200

    adjust elements colors & position

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div>
+    <div className="mx-auto flex max-w-2xl flex-col p-4">
       <Header />
     </div>
   );
