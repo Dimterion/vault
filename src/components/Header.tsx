fda2dad096997d84b3e629913050a3380f966e34
Dimterion <dimterion59@gmail.com>
2026-06-07 10:05:32 +0200

    add button component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 export default function Header() {
   return (
     <header className="flex items-center justify-between">
@@ -8,8 +10,8 @@ export default function Header() {
       <div className="flex flex-col gap-1">
         <span className="text-sm text-zinc-400">May 6 - May 16</span>
         <div className="flex items-center gap-3">
-          <button>Prev</button>
-          <button>Next</button>
+          <Button text="Prev"></Button>
+          <Button text="Next"></Button>
         </div>
       </div>
     </header>
