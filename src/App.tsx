f7797282834ad7ac1a34458340c2a191e6bfe526
Dimterion <dimterion59@gmail.com>
2026-06-09 07:40:54 +0100

    add HabitForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import HabitForm from "./components/HabitForm";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="mx-auto flex max-w-2xl flex-col p-4">
       <Header />
+      <HabitForm />
     </div>
   );
 }
---
+++ src/components/HabitForm.tsx
@@ -0,0 +1,10 @@
+import Button from "./Button";
+
+export default function HabitForm() {
+  return (
+    <form>
+      <input />
+      <Button>Add Habit</Button>
+    </form>
+  );
+}
