1069b9c41f02f64ca926c1c87373d71258c69461
Dimterion <dimterion59@gmail.com>
2026-06-11 07:29:23 +0100

    adjust HabitItem component

--- src/components/HabitItem.tsx
+++ src/components/HabitItem.tsx
@@ -3,5 +3,12 @@ type HabitItemProps = {
 };
 
 export default function HabitItem({ habit }: HabitItemProps) {
-  return <h1>{habit.name}</h1>;
+  return (
+    <div className="rounded-xl bg-zinc-800 p-4">
+      {" "}
+      <div className="flex flex-col items-center justify-between gap-3">
+        <span className="font-medium">{habit.name}</span>
+      </div>
+    </div>
+  );
 }
