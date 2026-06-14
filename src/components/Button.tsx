3e99084a0294f9727fd8c73ea4b8a2819ea9796d
Dimterion <dimterion59@gmail.com>
2026-06-14 08:19:08 +0100

    adjust Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,11 @@ function getVariantStyles(variant: Variant) {
   switch (variant) {
     case "primary":
       return "bg-violet-600 hover:violet-500";
+    case "secondary":
+      return "bg-zinc-700 hover:zinc-600 text-zinc-400";
+    case "ghost-destructive":
+      return "hover:bg-red-800 hover:text-red-200 text-red-800";
+
     default:
       throw new Error(`Invalid variant: ${variant satisfies never}`);
   }
