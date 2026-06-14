eb4bcabaeb372e30ad84a97f4a9eae235b209320
Dimterion <dimterion59@gmail.com>
2026-06-14 08:21:32 +0100

    adjust Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -24,7 +24,7 @@ export default function Button({ variant = "primary", ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="rounded bg-violet-600 px-2 py-1 transition-colors hover:bg-violet-500 disabled:cursor-not-allowed disabled:opacity-30"
+      className={`${getVariantStyles(variant)}rounded bg-violet-600 px-2 py-1 transition-colors hover:bg-violet-500 disabled:cursor-not-allowed disabled:opacity-30`}
     />
   );
 }
