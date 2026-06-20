636b57ba966785fcd0519d87659818766074ae1c
Dimterion <dimterion59@gmail.com>
2026-06-20 08:14:37 +0100

    adjust HabitProvider function

--- src/context/HabitProvider.tsx
+++ src/context/HabitProvider.tsx
@@ -1,11 +1,15 @@
-import { createContext } from "react";
+import { createContext, type ReactNode } from "react";
 
 type Context = {
   name: string;
 };
 
+type HabitProviderProps = {
+  children: ReactNode;
+};
+
 const HabitContext = createContext<null | Context>(null);
 
-export function HabitProvider() {
-  return <HabitContext value={{ name: "User" }}></HabitContext>;
+export function HabitProvider({ children }: HabitProviderProps) {
+  return <HabitContext value={{ name: "User" }}>{children}</HabitContext>;
 }
