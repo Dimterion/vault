710b8992620c184252a857d3f93b427f1bbb46ec
Dimterion <dimterion59@gmail.com>
2026-06-21 07:58:12 +0100

    adjust HabitProvider

--- src/context/HabitProvider.tsx
+++ src/context/HabitProvider.tsx
@@ -1,4 +1,5 @@
-import { createContext, type ReactNode } from "react";
+import { isSameDay } from "date-fns";
+import { createContext, useState, type ReactNode } from "react";
 
 type Habit = {
   id: string;
@@ -20,5 +21,33 @@ type HabitProviderProps = {
 const HabitContext = createContext<null | Context>(null);
 
 export function HabitProvider({ children }: HabitProviderProps) {
+  const [habits, setHabits] = useState<Habit[]>([]);
+
+  function addHabit(name: string) {
+    setHabits((curr) => [
+      ...curr,
+      { id: crypto.randomUUID(), name, completions: [] },
+    ]);
+  }
+
+  function deleteHabit(id: string) {
+    setHabits((curr) => curr.filter((h) => h.id !== id));
+  }
+
+  function toggleHabit(id: string, date: Date) {
+    setHabits((curr) =>
+      curr.map((h) => {
+        if (h.id !== id) return h;
+
+        const alreadyDone = h.completions.some((c) => isSameDay(c, date));
+        const completions = alreadyDone
+          ? h.completions.filter((c) => !isSameDay(c, date))
+          : [...h.completions, date];
+
+        return { ...h, completions };
+      }),
+    );
+  }
+
   return <HabitContext value={{ name: "User" }}>{children}</HabitContext>;
 }
