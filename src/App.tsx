10186aad12a29b319057c3a865bca19f903496a7
Dmitrii <dimterion59@gmail.com>
2026-06-29 10:55:16 +0200

    add Header comp

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
 import HabitTrackerPage from "./pages/habitTracker";
 import NotFoundPage from "./pages/notFound";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/habit-tracker" element={<HabitTrackerPage />}></Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/habit-tracker" element={<HabitTrackerPage />}></Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/habit-tracker">Habit Tracker</Link>
+    </nav>
+  );
 }
