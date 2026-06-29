09530d16c7397400bb350eb1e50749f0d9843430
Dmitrii <dimterion59@gmail.com>
2026-06-29 10:51:57 +0200

    add notFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
 import HabitTrackerPage from "./pages/habitTracker";
+import NotFoundPage from "./pages/notFound";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/habit-tracker" element={<HabitTrackerPage />}></Route>
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
