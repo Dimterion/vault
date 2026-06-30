7123796e4a89136aeae9285beb9848e56af3b087
Dmitrii <dimterion59@gmail.com>
2026-06-30 11:31:24 +0200

    minor links & dimensions adjustment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
 import { NavLink } from "react-router";
 
 export default function Header() {
+  const active = "text-gray-500";
+  const base = "hover:text-gray-500";
+
   return (
     <header className="flex flex-row items-center justify-between p-2">
       <h1 className="text-lg font-bold">Vault</h1>
       <nav className="inline-flex gap-2">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/habit-tracker">Habit Tracker</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/habit-tracker"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Habit Tracker
+        </NavLink>
       </nav>
     </header>
   );
