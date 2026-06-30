6a09e19faf3fc8cb64c113643576911649d7a90b
Dmitrii <dimterion59@gmail.com>
2026-06-30 11:18:57 +0200

    adjust elements structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/habit-tracker">Habit Tracker</Link>
+    <header className="flex flex-row items-center justify-between p-2">
+      <h1 className="text-lg font-bold">Vault</h1>
+      <nav className="inline-flex gap-2">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/habit-tracker">Habit Tracker</NavLink>
+      </nav>
     </header>
   );
 }
