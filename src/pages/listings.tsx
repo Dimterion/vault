f5121791be1ac42c1e302282fbf30dc063251ca5
Dmitrii <dimterion59@gmail.com>
2026-07-02 08:58:48 +0200

    adjust listings page structure

--- src/pages/listings.tsx
+++ src/pages/listings.tsx
@@ -2,9 +2,21 @@ import { listings } from "../assets/data/listings";
 
 export default function ListingsPage() {
   return (
-    <main>
+    <main className="flex flex-col gap-4 p-4">
+      <h2 className="text-center text-2xl font-bold">Listings</h2>
       {listings.map((listing) => (
-        <article key={listing.id}>{listing.name}</article>
+        <article key={listing.id}>
+          <h3 className="font-bold">{listing.name}</h3>
+          <p>{listing.description}</p>
+          <a
+            href={listing.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            Link
+          </a>
+        </article>
       ))}
     </main>
   );
