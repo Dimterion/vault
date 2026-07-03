759003bae0b890201484461e8164f27e3ea29e7c
Dmitrii <dimterion59@gmail.com>
2026-07-03 09:02:18 +0200

    add listings tags

--- src/pages/listings.tsx
+++ src/pages/listings.tsx
@@ -1,26 +1,40 @@
 import { listings } from "../assets/data/listings";
 
 export default function ListingsPage() {
+  const tags = [
+    ...new Set(
+      listings.flatMap((listing) =>
+        listing.tags.split(",").map((tag) => tag.trim()),
+      ),
+    ),
+  ];
+
   return (
     <main className="flex flex-col items-center gap-4 p-4">
       <h2 className="text-2xl font-bold">Listings</h2>
-      {listings.map((listing) => (
-        <article
-          key={listing.id}
-          className="w-full max-w-2xl rounded bg-zinc-800 p-4"
-        >
-          <h3 className="font-bold">{listing.name}</h3>
-          <p>{listing.description}</p>
-          <a
-            href={listing.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:underline"
-          >
-            Link
-          </a>
-        </article>
-      ))}
+      <section>
+        {tags.map((tag) => (
+          <button className="mx-2 cursor-pointer rounded bg-zinc-800 px-4 py-2 hover:bg-zinc-700">
+            {tag}
+          </button>
+        ))}
+      </section>
+      <section className="flex w-full max-w-2xl flex-col gap-4">
+        {listings.map((listing) => (
+          <article key={listing.id} className="rounded bg-zinc-800 p-4">
+            <h3 className="font-bold">{listing.name}</h3>
+            <p>{listing.description}</p>
+            <a
+              href={listing.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Link
+            </a>
+          </article>
+        ))}
+      </section>
     </main>
   );
 }
