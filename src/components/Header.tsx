2d604af17e5ea46e0052c47dd047a5d7ab1baaca
Dmitrii <dimterion59@gmail.com>
2026-07-08 08:55:42 +0200

    add initial small screen header version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router";
 
 export default function Header() {
+  const [showNav, setShowNav] = useState(false);
+
   const active = "";
   const base = "text-gray-500 hover:text-white";
 
   return (
-    <header className="flex flex-row items-center justify-between bg-zinc-800 p-2">
+    <header className="flex flex-col sm:items-center justify-between bg-zinc-800 p-2 sm:flex-row">
       <h1 className="text-lg font-bold">
         <Link to="/">Vault</Link>
       </h1>
-      <nav className="inline-flex gap-2">
+      <nav className="flex flex-row flex-wrap justify-center gap-2 sm:hidden">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/habit-tracker"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Habit Tracker
+        </NavLink>
+        <NavLink
+          to="/listings"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Listings
+        </NavLink>
+      </nav>
+      <nav className="hidden gap-2 sm:inline-flex">
         <NavLink
           to="/"
           className={({ isActive }) => (isActive ? active : base)}
