24cb9ae0e45058abb4b2441d284ebfe677b36903
Dmitrii <dimterion59@gmail.com>
2026-07-08 09:01:00 +0200

    adjust header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router";
+import Button from "./Button";
 
 export default function Header() {
   const [showNav, setShowNav] = useState(false);
@@ -8,30 +9,38 @@ export default function Header() {
   const base = "text-gray-500 hover:text-white";
 
   return (
-    <header className="flex flex-col sm:items-center justify-between bg-zinc-800 p-2 sm:flex-row">
+    <header className="relative flex flex-col justify-between bg-zinc-800 p-2 sm:flex-row sm:items-center">
       <h1 className="text-lg font-bold">
         <Link to="/">Vault</Link>
       </h1>
-      <nav className="flex flex-row flex-wrap justify-center gap-2 sm:hidden">
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? active : base)}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/habit-tracker"
-          className={({ isActive }) => (isActive ? active : base)}
-        >
-          Habit Tracker
-        </NavLink>
-        <NavLink
-          to="/listings"
-          className={({ isActive }) => (isActive ? active : base)}
-        >
-          Listings
-        </NavLink>
-      </nav>
+      <Button
+        onClick={() => setShowNav(!showNav)}
+        className="absolute top-0 right-0 sm:hidden"
+      >
+        {showNav ? "X" : "^"}
+      </Button>
+      {showNav && (
+        <nav className="flex flex-row flex-wrap justify-center gap-2 sm:hidden">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/habit-tracker"
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            Habit Tracker
+          </NavLink>
+          <NavLink
+            to="/listings"
+            className={({ isActive }) => (isActive ? active : base)}
+          >
+            Listings
+          </NavLink>
+        </nav>
+      )}
       <nav className="hidden gap-2 sm:inline-flex">
         <NavLink
           to="/"
