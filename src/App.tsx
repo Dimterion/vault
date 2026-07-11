0025bd0492241c34278f62a9d7c127d793b3d19e
Dmitrii <dimterion59@gmail.com>
2026-07-11 09:33:15 +0200

    add about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
 import HabitTrackerPage from "./pages/habitTracker";
 import ListingsPage from "./pages/listings";
+import AboutPage from "./pages/about";
 import NotFoundPage from "./pages/notFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/habit-tracker" element={<HabitTrackerPage />}></Route>
         <Route path="/listings" element={<ListingsPage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import Hero from "../components/Hero";
+
+export default function AboutPage() {
+  return (
+    <main className="mx-auto p-10">
+      <Hero title="About page" text="About page description." />
+    </main>
+  );
+}
