859c6182c5a23e65abfcb8d657cd318cf75bd37a
Dmitrii <dimterion59@gmail.com>
2026-07-15 13:33:35 +0200

    adjust hero component & about page

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,7 @@ type HeroProps = {
 
 export default function Hero({ title, text, image }: HeroProps) {
   return (
-    <section className="flex w-64 flex-col items-center gap-4 rounded bg-zinc-800 p-6 sm:w-xl">
+    <section className="mx-auto flex w-56 flex-col items-center gap-4 rounded bg-zinc-800 p-6 text-center sm:w-xl">
       {image && (
         <img src={image} alt="Hero section image." className="rounded" />
       )}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,8 +2,11 @@ import Hero from "../components/Hero";
 
 export default function AboutPage() {
   return (
-    <div className="mx-auto p-10">
-      <Hero title="About page" text="About page description." />
+    <div className="flex min-h-full flex-1 items-center justify-center px-4 py-10">
+      <Hero
+        title="About Vault app"
+        text="Vault is an app to organize your activities."
+      />
     </div>
   );
 }
